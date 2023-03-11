846d82f4dd0963860bf7d36f2b2fe97dc85f6340
carolinemartins <caroline.martins.soares09@gmail.com>
2023-03-11 20:46:33 -0300

    create band controller

---
+++ src/controller/BandController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { BandBusiness } from "../business/BandBusiness";
+
+const bandBusiness = new BandBusiness();
+
+export class BandController {
+    create = async (req: Request, res: Response) => {
+        try {
+            const idAutorization = req.headers.authorization as string;
+            const { name, musicGenre, responsible } = req.body
+
+            const insertBand = {
+                name: name,
+                musicGenre: musicGenre,
+                responsible: responsible,
+                idAutorization: idAutorization
+            }
+            await bandBusiness.createBand(insertBand)
+
+            res.status(201).send({ message: "Band created successfully!" })
+        } catch (error: any) {
+            throw new Error(error.message || error.sqlMessage);
+        }
+
+    }
+}
