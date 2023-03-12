668fe1bedf033abe340c092c573ca84083258143
carolinemartins <caroline.martins.soares09@gmail.com>
2023-03-11 21:45:10 -0300

    dataBase shows

---
+++ src/data/ShowDatabase.ts
@@ -0,0 +1,22 @@
+import { Show } from "../model/Show";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class ShowDatabase extends BaseDatabase{
+    private static TABLE_NAME = "shows";
+
+    create = async({id, weekDay, startTime, endTime, bandId}:Show)=>{
+        try{
+            await this.getConnection()
+            .insert({
+                id:id,
+                week_day:weekDay,
+                start_time:startTime,
+                end_time:endTime,
+                band_id:bandId
+            }).into(ShowDatabase.TABLE_NAME)
+
+        }catch(error:any){
+            throw new Error(error.sqlMessage || error.message);
+        }
+    }
+}
---
+++ src/model/Show.ts
@@ -0,0 +1,7 @@
+export interface Show {
+    id: string,
+    weekDay: string,
+    startTime: string,
+    endTime: string,
+    bandId: string
+}
