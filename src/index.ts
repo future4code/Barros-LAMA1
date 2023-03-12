82c0caa624cef49a1704ec21f5b26a94b3320ae1
carolinemartins <caroline.martins.soares09@gmail.com>
2023-03-12 14:03:29 -0300

    port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { bandRouter } from "./routes/bandRouter";
 import { showRouter } from "./routes/showRouter";
+
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
@@ -16,7 +18,7 @@ app.use("/band", bandRouter);
 
 app.use("/show", showRouter);
 
-const server = app.listen(3003, () => {
+const server = app.listen(process.env.DB_PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
