acb8a72871a19c4c52655ee8e091a04d50575487
carolinemartins <caroline.martins.soares09@gmail.com>
2023-03-13 13:07:57 -0300

    app,index separado

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import dotenv from "dotenv";
-import { AddressInfo } from "net";
-import express from "express";
+import {app} from "./app";
 import { userRouter } from "./routes/userRouter";
 import { bandRouter } from "./routes/bandRouter";
 import { showRouter } from "./routes/showRouter";
 
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
 
 ///ENDPOINT USER
 app.use("/user", userRouter);
@@ -18,11 +11,3 @@ app.use("/band", bandRouter);
 
 app.use("/show", showRouter);
 
-const server = app.listen(process.env.DB_PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Servidor rodando em http://localhost:${address.port}`);
-  } else {
-    console.error(`Falha ao rodar o servidor.`);
-  }
-});
